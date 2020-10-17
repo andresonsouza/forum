7ba6745ec38d72c53dc3cb72f26dda0d73f3914f
Andreson Souza <andresonjazz@gmail.com>
2020-10-17 13:50:18 -0300

    fix: devolva uma classe ao invés de uma entitade jpa

--- src/main/java/br/com/alura/forum/controller/HelloController.java
+++ src/main/java/br/com/alura/forum/controller/HelloController.java
@@ -4,10 +4,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // Quando usada no lugar da anotação @Controller não é mais necessário usar a anotação @ResponseBody
 public class HelloController {
 
 	@RequestMapping("/")
+	@ResponseBody // Se não for inserida essa anotação o spring considera que a navegação será feita para uma página
 	public String hello() {
 		return "Hello World!";
 	}
--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -4,20 +4,19 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
-@RestController // Quando usada no lugar da anotação @Controller não é mais necessário usar a anotação @ResponseBody
+@RestController
 public class TopicosController {
 	
 	@RequestMapping("/topicos")
-	@ResponseBody // Se não for inserida essa anotação o spring considera que a navegação será feita para uma página
-	public List<Topico> lista() {
-		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
+	public List<TopicoDto> lista() {
+		Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
 	
-		return Arrays.asList(topico,topico,topico);
+		return TopicoDto.converter( Arrays.asList(topico,topico,topico));
 	}
 }
